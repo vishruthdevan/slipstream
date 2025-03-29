07033f3639d2a76c72efd519155e27d94ee71729
Vishruth Devan <vishruthdevan@gmail.com>
2025-03-29 18:57:15 -0400

    Create route for race results

---
+++ routes/race_results.py
@@ -0,0 +1,40 @@
+from flask import g, render_template
+from sqlalchemy import text
+
+from routes import race_results_bp
+
+
+@race_results_bp.route("/<int:race_id>/")
+def race_results(race_id):
+    """Retrieve and display details for a specific race."""
+    query = text(
+        """
+        SELECT rr.position, rr.points, d.firstname, d.lastname, c.name as constructor_name, r.name AS race_name, r.date
+        FROM raceresults rr
+        JOIN race r ON rr.raceid = r.raceid AND r.raceid = :race_id
+        JOIN driver d ON rr.driverid = d.driverid
+        JOIN constructor c ON rr.constructorid = c.constructorid
+        ORDER BY rr.position ASC
+    """
+    )
+
+    cursor = g.conn.execute(query, {"race_id": race_id})
+    race_results_data = []
+
+    for row in cursor:
+        race_results_data.append(
+            {
+                "position": row[0],
+                "points": row[1],
+                "first_name": row[2],
+                "last_name": row[3],
+                "constructor_name": row[4],
+                "race_name": row[5],
+                "date": row[6],
+            }
+        )
+    cursor.close()
+
+    if race_results_data is not None:
+        return render_template("race_results.html", race_results=race_results_data)
+    return render_template("not_found.html"), 404
