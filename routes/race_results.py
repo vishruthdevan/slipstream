0517d08fb07bc5561e4557656b3e2788b6004a31
Vishesh Arora <>
2025-04-02 22:25:25 -0400

    Add driver id

--- routes/race_results.py
+++ routes/race_results.py
@@ -9,7 +9,7 @@ def race_results(race_id):
     """Retrieve and display details for a specific race."""
     query = text(
         """
-        SELECT rr.position, rr.points, d.firstname, d.lastname, c.constructorid, c.name as constructor_name, r.name AS race_name, r.date
+        SELECT rr.position, rr.points, d.driverid, d.firstname, d.lastname, c.constructorid, c.name as constructor_name, r.name AS race_name, r.date
         FROM raceresults rr
         JOIN race r ON rr.raceid = r.raceid AND r.raceid = :race_id
         JOIN driver d ON rr.driverid = d.driverid
@@ -26,12 +26,13 @@ def race_results(race_id):
             {
                 "position": row[0],
                 "points": row[1],
-                "first_name": row[2],
-                "last_name": row[3],
-                "constructor_id": row[4],
-                "constructor_name": row[5],
-                "race_name": row[6],
-                "date": row[7],
+                "driver_id": row[2],
+                "first_name": row[3],
+                "last_name": row[4],
+                "constructor_id": row[5],
+                "constructor_name": row[6],
+                "race_name": row[7],
+                "date": row[8],
             }
         )
     cursor.close()
